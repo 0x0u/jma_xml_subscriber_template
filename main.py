c928d30649519f2cdac0fa00ccc70dc013071539
0x0u <m0zurillex@gmail.com>
2020-04-01 18:23:55 +0900

    署名検証方法の変更

--- main.py
+++ main.py
@@ -26,18 +26,16 @@ def get():
 
 @app.route('/sub', methods=['POST'])
 def post():
-    sha1 = request.headers.get('X-Hub-Signature')
-    if sha1:
-        data = request.get_data(as_text=True)
-        sig = 'sha1=' + hmac.new(bytes(VERIFY_TOKEN, 'UTF-8'), bytes(data, 'UTF-8'), hashlib.sha1).hexdigest()
-        if sig == sha1:
+    signature = request.headers["X-Hub-Signature"]
+    body = request.get_data(as_text=True)
+    hash_ = 'sha1=' + hmac.new(bytes(VERIFY_TOKEN, 'UTF-8'),
+                               bytes(body, 'UTF-8'), hashlib.sha1).hexdigest()
+    if signature == hash_:
 
-            # 処理を書く
-            # POSTされたAtomはdataに入っているのでBeautifulSoupなどでXMLをパースして遊ぶ
+        # 処理を書く
+        # POSTされたAtomはdataに入っているのでBeautifulSoupなどでXMLをパースして遊ぶ
 
-            return Response(response='ok', status=200)
-        else:
-            return Response(response='Bad request!', status=404)
+        return Response(response='ok', status=200)
     else:
         return Response(response='Bad request!', status=404)
 
